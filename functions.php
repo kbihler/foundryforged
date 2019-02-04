ea000a51b61afed2e67db3986742b8fc5df6760a
Kyle Bihler <kyle@bausermangroup.com>
2019-02-04 10:16:23 -0800

    end of day

--- functions.php
+++ functions.php
@@ -117,11 +117,78 @@ function foundryforged_widgets_init() {
 add_action( 'widgets_init', 'foundryforged_widgets_init' );
 
 /**
+ * Register custom fonts.
+ */
+function foundryforged_fonts_url() {
+	$fonts_url = '';
+
+	/*
+	 * Translators: If there are characters in your language that are not
+	 * supported by Anton, Roboto Condensed, and Roboto, translate this to 'off'. Do not translate
+	 * into your own language.
+	 * https://fonts.googleapis.com/css?family=Anton|Roboto+Condensed:400,700|Roboto:300,400
+	 */
+	$anton = _x( 'on', 'Anton font: on or off', 'foundryforged' );
+	$roboto_condensed = _x( 'on', 'Roboto Condensed font: on or off', 'foundryforged' );
+	$roboto = _x( 'on', 'Roboto font: on or off', 'foundryforged' );
+
+	$font_families = array();
+
+	if ( 'off' !== $anton ) {
+
+		$font_families[] = 'Anton';
+	}
+
+	if ( 'off' !== $roboto_condensed ) {
+
+		$font_families[] = 'Roboto+Condensed:400,700';
+	}
+
+	if ( 'off' !== $roboto ) {
+
+		$font_families[] = 'Roboto:300,400';
+	}
+
+	if ( in_array( 'on', array($anton, $roboto_condensed, $roboto)) ) {
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Foundry Forged 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function foundryforged_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'foundryforged-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'foundryforged_resource_hints', 10, 2 );
+
+/**
  * Enqueue scripts and styles.
  */
 function foundryforged_scripts() {
 	// enqueue google fonts. if changed adjust sass > variables-site > _typography.scss
-  wp_enqueue_style('foundryforged-fonts', 'https://fonts.googleapis.com/css?family=Anton|Roboto+Condensed:400,700|Roboto:300,400');
+  wp_enqueue_style('foundryforged-fonts', foundryforged_fonts_url());
 	
 	wp_enqueue_style( 'foundryforged-style', get_stylesheet_uri() );
 
